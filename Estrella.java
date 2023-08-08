077d2aac31332a79244740ae347e0290b2815e0b
JuanViau <via23404@uvg.edu.gt>
2023-08-07 22:58:26 -0600

    Update Estrella.java

--- Estrella.java
+++ Estrella.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Estrella extends Actor
+public class Estrella extends Actor //JPARDO
 {
     /**
      * Act - do whatever the Estrella wants to do. This method is called whenever
